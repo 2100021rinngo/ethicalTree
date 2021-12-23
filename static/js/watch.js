fe52d675826c6b70d23981977e4581c339170bc2
城ノ内2100021053 <2100021053@gs.tokushima-ec.ed.jp>
2021-12-24 07:54:48 +0900

    continue to make branch

--- static/js/watch.js
+++ static/js/watch.js
@@ -1,14 +1,27 @@
-onload = reizeWindow;
+onload = initialize;
 
 let header;
 let work;
-let rate;
-function reizeWindow()
+let main;
+async function initialize()
 {
     header = document.getElementById("header");
-    work = document.getElementById("main");
-    rate = window.innerWidth / 1000;
-    rate = (rate == 1) ? 1 : rate;
-    work.style.transform = "scale(" + rate + "," + rate + ")";
+    work = document.getElementById("work");
+
+    main = document.getElementsByTagName("main")[0];
+
+    reizeWindow();
+}
+
+// let rate;
+async function reizeWindow()
+{
+    // work = document.getElementById("main");
+    // rate = window.innerWidth / 1000;
+    // rate = (rate == 1) ? 1 : rate;
+    // work.style.transform = "scale(" + rate + "," + rate + ")";
+    main.style.width = `${window.innerWidth}px`;
     header.style.width = `${window.innerWidth}px`;
+
+    console.log(main.style.width);
 }
